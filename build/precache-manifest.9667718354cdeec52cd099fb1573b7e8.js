6427a2ac73f79ea6c80e9cbf7cd13b50090523d3
codersipayi <jeevannotes@gmail.com>
2020-11-04 13:00:52 +0530

    testing deployment

---
+++ build/precache-manifest.9667718354cdeec52cd099fb1573b7e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91c04be5c269aedaf1f49b3f5f1a7b7",
+    "url": "/weather_application/index.html"
+  },
+  {
+    "revision": "f64f56271413872efc96",
+    "url": "/weather_application/static/css/main.488fbbe1.chunk.css"
+  },
+  {
+    "revision": "d36808d39f2ce0ac7949",
+    "url": "/weather_application/static/js/2.94ec2d86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_application/static/js/2.94ec2d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f64f56271413872efc96",
+    "url": "/weather_application/static/js/main.1d243007.chunk.js"
+  },
+  {
+    "revision": "5507c1f0a84ec646e25c",
+    "url": "/weather_application/static/js/runtime-main.eb34185e.js"
+  }
+]);
